ac5e30dc4cf2eef9b48a802f2681f1df8aa684c7
Ismail Diari <diari.ismail@gmail.com>
2022-07-22 22:22:21 +0200

    feat(angular): Add routing + two components

---
+++ MyLegacyApp/Scripts/Angular/src/app/hello-world/component-two/component-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-two',
+  templateUrl: './component-two.component.html',
+  styleUrls: ['./component-two.component.scss']
+})
+export class ComponentTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- MyLegacyApp/Scripts/Angular/src/app/hello-world/hello-world.module.ts
+++ MyLegacyApp/Scripts/Angular/src/app/hello-world/hello-world.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: 'one', component: ComponentOneComponent },
+  { path: 'two', component: ComponentTwoComponent },
+  { path: '', redirectTo: 'one', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    HelloWorldComponent
+    HelloWorldComponent,
+    ComponentOneComponent,
+    ComponentTwoComponent
   ],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  exports: [HelloWorldComponent],
+  exports: [HelloWorldComponent, ComponentOneComponent, ComponentTwoComponent],
+  providers:[{provide: APP_BASE_HREF, useValue : '' }],
   bootstrap:[HelloWorldComponent]
 })
 export class HelloWorldModule { }
